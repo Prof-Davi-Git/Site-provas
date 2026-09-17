44209df48d29ba15d76537b8a1462e0ea90ce6d5
Prof-Davi-Git <davigodoi@prof.educacao.sp.gov.br>
2026-09-17 00:49:34 -0300

    Atualiza cache do modo de teste

--- recuperacao.js
+++ recuperacao.js
@@ -376,7 +376,7 @@ window.addEventListener("online", () => verificarInternetReal());
 // Registra o Service Worker para permitir que a página continue carregando offline.
 if ("serviceWorker" in navigator) {
   window.addEventListener("load", () => {
-    navigator.serviceWorker.register("./sw.js?v=20260917-3").catch(() => {});
+    navigator.serviceWorker.register("./sw.js?v=20260917-4").catch(() => {});
   });
 }
 
